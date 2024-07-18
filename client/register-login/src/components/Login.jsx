ad0873455d5c6d12caa2c1bdd22edf8c630e9d75
Harshit Mourya <mouryaharshit03@gmail.com>
2024-07-18 16:51:23 +0530

    login page video and logo

--- client/register-login/src/components/Login.jsx
+++ client/register-login/src/components/Login.jsx
@@ -1,11 +1,33 @@
+import video from "../Login assets/video.mp4";
+import logo from "../Login assets/logo.png";
+import "./Login.css";
+import { Link } from "react-router-dom";
+
 export default function Login() {
   return (
-    <div>
-      <a href="/register">To Register</a>
-      <br />
-      <a href="/dashboard">To Dashboard</a>
-      <br />
-      This is Login Page
+    <div className="flex loginPage">
+      <div className="container flex">
+        <div className="video">
+          <video src={video} autoPlay muted loop></video>
+          <div className="text">
+            <h2>Create And Sell Extraordinary Products</h2>
+            <p>Adopt the piece of nature!</p>
+          </div>
+          <div className="footer flex">
+            <span>Don't have an account?</span>
+            <Link to={"/register"}>
+              <button>Sign Up</button>
+            </Link>
+          </div>
+        </div>
+
+        <div className="form flex">
+          <div className="header">
+            <img src={logo} alt="Logo" />
+            <h3>Welcome Back!</h3>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
