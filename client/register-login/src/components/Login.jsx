3520140bb256cca2f05605fd3d82d926727427d1
Harshit Mourya <mouryaharshit03@gmail.com>
2024-07-19 20:05:46 +0530

    login page designing

--- client/register-login/src/components/Login.jsx
+++ client/register-login/src/components/Login.jsx
@@ -1,19 +1,23 @@
 import video from "../Login assets/video.mp4";
 import logo from "../Login assets/logo.png";
+import { FaUser } from "react-icons/fa";
+import { BsShieldLockFill } from "react-icons/bs";
+import { TbLogin2 } from "react-icons/tb";
+
 import "./Login.css";
 import { Link } from "react-router-dom";
 
 export default function Login() {
   return (
-    <div className="flex loginPage">
-      <div className="container flex">
+    <div className="loginPage">
+      <div className="container">
         <div className="video">
           <video src={video} autoPlay muted loop></video>
           <div className="text">
-            <h2>Create And Sell Extraordinary Products</h2>
+            <h2 className="title">Create And Sell Extraordinary Products</h2>
             <p>Adopt the piece of nature!</p>
           </div>
-          <div className="footer flex">
+          <div className="footer">
             <span>Don't have an account?</span>
             <Link to={"/register"}>
               <button>Sign Up</button>
@@ -21,11 +25,28 @@ export default function Login() {
           </div>
         </div>
 
-        <div className="form flex">
+        <div className="form-box">
           <div className="header">
             <img src={logo} alt="Logo" />
             <h3>Welcome Back!</h3>
           </div>
+          <form action="" className="form">
+            <span></span>
+            <label htmlFor="username">Username</label>
+            <FaUser />
+            <input type="text" id="username" placeholder="Enter Username" />
+            <label htmlFor="password">Password</label>
+            <BsShieldLockFill />
+            <input type="password" id="password" placeholder="Enter Password" />
+            <button type="submit">
+              Login
+              <TbLogin2 />
+            </button>
+
+            <span>
+              Forget your password? <a href="">Click here</a>
+            </span>
+          </form>
         </div>
       </div>
     </div>
