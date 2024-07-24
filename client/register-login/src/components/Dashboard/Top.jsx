eafbfaae2783011a71b6984ba2d3e36e7d46be5d
Harshit Mourya <mouryaharshit03@gmail.com>
2024-07-24 19:59:17 +0530

    Top section of Body

--- client/register-login/src/components/Dashboard/Top.jsx
+++ client/register-login/src/components/Dashboard/Top.jsx
@@ -2,8 +2,11 @@ import "./Top.css";
 import { BiSearchAlt } from "react-icons/bi";
 import { RiMessage2Line } from "react-icons/ri";
 import { IoIosNotificationsOutline } from "react-icons/io";
+import { GoArrowRight } from "react-icons/go";
+
 import video from "./video.mp4";
 import adminImg from "./jay.jpeg";
+import plant1 from "./plant1.png";
 
 export default function Top() {
   return (
@@ -36,6 +39,27 @@ export default function Top() {
           </div>
           <div className="videoDiv">
             <video src={video} autoPlay muted loop></video>
+          </div>
+        </div>
+        <div className="leftCard">
+          <div className="main">
+            <div className="textDiv">
+              <h1>My Stat</h1>
+              <div className="flex">
+                <span>
+                  Today <br /> <small>4 Orders</small>
+                </span>
+                <span>
+                  This Month <br /> <small>124 Orders</small>
+                </span>
+              </div>
+              <span className="flex link">
+                Go to my orders <GoArrowRight />
+              </span>
+            </div>
+            <div className="imgCard">
+              <img src={plant1} alt="Image" />
+            </div>
           </div>
         </div>
       </div>
